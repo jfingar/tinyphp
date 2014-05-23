f08115adbbf2ce4c1ba5439de9566ade81733de3
Jason Fingar <jfingar@localhost.localdomain>
2014-05-22 18:11:10 -0700

    finish data modeler

--- application/libraries/TinyPHP/Db/DataModeler.php
+++ application/libraries/TinyPHP/Db/DataModeler.php
@@ -23,6 +23,7 @@ class DataModeler
         foreach($tables as $table){
             self::createTableClass($table);
             self::createModelClass($table);
+            self::createMapperClass($table);
         }
     }
     
@@ -49,6 +50,7 @@ HERE;
         $modelFileName = self::getFilename($tableName);
         $className = self::getClassname($tableName);
         $phpFile = fopen($modelFileName, 'w');
+	$columnRows = self::getTableColumns($tableName);
         $textString = <<<HERE
 <?php
 namespace Models;
@@ -56,43 +58,157 @@ class $className
 {
 
 HERE;
-        $sql = "SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . self::$_schemaName . "' AND TABLE_NAME = '" . $tableName  . "'";
-		$dbAdapter = Adapter::GetMysqlAdapter();
-		$statement = $dbAdapter->query($sql);
-		$columnRows = $statement->fetchAll();
-		
-		foreach($columnRows as $column){
-			$property = self::getProperty($column['COLUMN_NAME']);
-			$textString .= <<<HERE
+	
+	foreach($columnRows as $column){
+            $property = self::getProperty($column['COLUMN_NAME']);
+            $textString .= <<<HERE
     private $property;\r\n
 HERE;
-		}
+	}
 		
-		$textString .= "\r\n";
+	$textString .= "\r\n";
 		
-		foreach($columnRows as $column){
-			$methodName = str_replace("_", " ",$column['COLUMN_NAME']);
-			$methodName = ucwords($methodName);
-			$getterMethodName = 'get' . str_replace(" ", "", $methodName);
-			$setterMethodName = 'set' . str_replace(" ", "", $methodName);
-			$property = substr(self::getProperty($column['COLUMN_NAME']),1);
-			$textString .= <<<HERE
+	foreach($columnRows as $column){
+            $methodName = str_replace("_", " ",$column['COLUMN_NAME']);
+            $methodName = ucwords($methodName);
+            $getterMethodName = 'get' . str_replace(" ", "", $methodName);
+            $setterMethodName = 'set' . str_replace(" ", "", $methodName);
+            $property = substr(self::getProperty($column['COLUMN_NAME']),1);
+            $textString .= <<<HERE
     public function $getterMethodName()
-	{
-		return \$this->$property;
+    {
+	return \$this->$property;
     }
 
     public function $setterMethodName(\$$property)
-	{
-		\$this->$property = \$$property;
-		return \$this;
-	}
+    {
+	\$this->$property = \$$property;
+	return \$this;
+    }
 
 
 HERE;
-		}
+	}
 		
-		$textString .= '}';
+	$textString .= '}';
+		
+        fwrite($phpFile, $textString);
+        fclose($phpFile);
+    }
+    
+    private static function createMapperClass($tableName)
+    {
+        $modelFileName = self::getFilename($tableName, 'mappers');
+        $className = self::getClassname($tableName);
+        $phpFile = fopen($modelFileName, 'w');
+	$columnNames = self::getTableColumns($tableName);
+        $textString = <<<HERE
+<?php
+namespace Models\Mappers;
+use Libraries\TinyPHP\Db\IMappable;
+use Models\\$className AS {$className}_Model;
+use Models\Tables\\$className AS {$className}_Table;
+class $className implements IMappable
+{
+    private \$_table;
+        
+    public function getTable()
+    {
+        if(\$this->_table === null){
+            \$this->_table = new {$className}_Table;
+        }
+        return \$this->_table;
+    }
+            
+    public function find(\$pk)
+    {
+        \$row = \$this->getTable()->find(\$pk);
+        if(!\$row){
+            return false;
+        }
+        \$obj = new {$className}_Model();
+        \$this->_map(\$obj, \$row);
+        return \$obj;
+    }
+        
+    public function fetchRow(array \$params = array())
+    {
+        \$row = \$this->getTable()->fetchRow(\$params);
+        if(!\$row){
+            return false;
+        }
+        \$obj = new {$className}_Model();
+        \$this->_map(\$obj, \$row);
+        return \$obj;
+    }
+        
+    public function fetchAll(array \$params = array(), \$orderBy = '', \$limit = '')
+    {
+        \$rowSet = \$this->getTable()->fetchAll(\$params, \$orderBy, \$limit);
+        \$resultSet = array();
+        foreach(\$rowSet as \$row){
+            \$obj = new {$className}_Model();
+            \$this->_map(\$obj, \$row);
+            \$resultSet[] = \$obj;
+        }
+        return \$resultSet;
+    }
+            
+    public function save(\$obj)
+    {
+        \$params = array(
+
+HERE;
+	foreach($columnNames as $column){
+	    $columnName = $column['COLUMN_NAME'];
+	    $methodName = str_replace("_", " ",$columnName);
+            $methodName = ucwords($methodName);
+            $getterMethodName = 'get' . str_replace(" ", "", $methodName);
+	    $textString .= <<<HERE
+	    "$columnName" => \$obj->{$getterMethodName}(),
+
+HERE;
+	}
+	$textString = substr($textString, 0, -3) . "\r\n\t);\r\n\r\n";
+        $textString .= <<<HERE
+	\$pk = (int) \$obj->getId();
+        if(\$pk > 0){
+            \$this->getTable()->update(\$params, array("id" => \$pk));
+        }else{
+            \$pk = \$this->getTable()->insert(\$params);
+            \$obj->setId(\$pk);
+        }
+    }
+		
+    public function delete(\$obj)
+    {
+        \$id = \$obj->getId();
+        if(!\$id){
+            return false;
+        }
+        \$this->getTable()->delete(array("id" => \$id));
+    }
+		
+    public function _map(\$obj, \$row)
+    {
+
+HERE;
+	
+	foreach($columnNames as $column){
+	    $columnName = $column['COLUMN_NAME'];
+	    $methodName = str_replace("_", " ",$columnName);
+            $methodName = ucwords($methodName);
+            $setterMethodName = 'set' . str_replace(" ", "", $methodName);
+	    $textString .= <<<HERE
+	\$obj->{$setterMethodName}(\$row['$columnName']);
+
+HERE;
+	}
+	
+	$textString .= <<<HERE
+    }
+}
+HERE;
 		
         fwrite($phpFile, $textString);
         fclose($phpFile);
@@ -123,12 +239,22 @@ HERE;
         return $className;
     }
 	
-	private static function getProperty($columnName)
-	{
-		$property = str_replace("_", " ", $columnName);
-		$property = ucwords($property);
-		$property = str_replace(" ","",$property);
-		$property = "\$_" . lcfirst($property);
-		return $property;
-	}
+    private static function getProperty($columnName)
+    {
+	    $property = str_replace("_", " ", $columnName);
+	    $property = ucwords($property);
+	    $property = str_replace(" ","",$property);
+	    $property = "\$_" . lcfirst($property);
+	    return $property;
+    }
+    
+    private static function getTableColumns($tableName)
+    {
+	$sql = "SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . self::$_schemaName . "' AND TABLE_NAME = '" . $tableName  . "'";
+	$dbAdapter = Adapter::GetMysqlAdapter();
+	$statement = $dbAdapter->query($sql);
+	$columnRows = $statement->fetchAll();
+	return $columnRows;
+    }
+	
 }
