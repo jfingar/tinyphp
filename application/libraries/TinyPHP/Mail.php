e77f233ee825859672f0631d59b5a6c78839b1f9
jfingar <jfingar@gmail.com>
2015-03-24 10:43:11 -0700

    updated the Mail class to include smtp features

--- application/libraries/TinyPHP/Mail.php
+++ application/libraries/TinyPHP/Mail.php
@@ -1,7 +1,9 @@
 <?php
 namespace Libraries\TinyPHP;
+
 use \PHPMailer;
 use \Exception;
+
 class Mail
 {
     private $recipients = array();
@@ -11,54 +13,56 @@ class Mail
     private $subject;
     private $body;
     private $attachments = array();
-    private $transportType = 'sendmail';
-    private static $_smtpHost;
-    private static $_smtpPort;
-    private static $_smtpUsername;
-    private static $_smtpPassword;
-    
+    private $transportType = 'smtp';
+    private $replyTo;
+
     public function addRecipient($recipientEmail)
     {
         $this->recipients[] = $recipientEmail;
     }
-    
-    public function setFrom($fromEmail,$displayName = null)
+
+    public function setFrom($fromEmail, $displayName = null)
     {
         $this->from = $fromEmail;
         if($displayName){
             $this->fromDisplay = $displayName;
         }
     }
-    
+
     public function setType($type)
     {
         $this->type = $type;
     }
-    
+
     public function setSubject($subject)
     {
         $this->subject = $subject;
     }
-    
+
     public function setBody($body)
     {
         $this->body = $body;
     }
-    
+
     public function addAttachment($attachmentPath)
     {
         $this->attachments[] = $attachmentPath;
     }
-    
+
     public function setTransportType($transportType)
     {
         $this->transportType = $transportType;
     }
     
+    public function setReplyTo($replyTo)
+    {
+        $this->replyTo = $replyTo;
+    }
+
     public function send()
     {
         $mail = new PHPMailer();
-        $mail->SetFrom($this->from,$this->fromDisplay);
+        $mail->SetFrom($this->from, $this->fromDisplay);
         if(empty($this->recipients)){
             throw new Exception("Please add at least 1 recipient!");
         }
@@ -68,22 +72,28 @@ class Mail
         $mail->Subject = $this->subject;
         $mail->MsgHTML($this->body);
         
+        if($this->transportType == 'smtp'){
+            $mail->SMTPAuth = true;
+            $mail->isSMTP();
+            $config = Application::$config;
+            $mail->Host =  $config['smtpHost'];
+            $mail->Port = $config['smtpPort'];
+            $mail->Username = $config['smtpUsername'];
+            $mail->Password = $config['smtpPassword'];
+        }
+        
         if(!empty($this->attachments)){
             foreach($this->attachments as $attachmentPath){
-               $mail->AddAttachment($attachmentPath);
+                $mail->AddAttachment($attachmentPath);
             }
         }
+        
+        if($this->replyTo){
+            $mail->addReplyTo($this->replyTo);
+        }
+        
         if(!$mail->Send()){
             throw new Exception("Mailer Error: " . $mail->ErrorInfo);
         }
-    }
-    
-    private static function getSmtpCredentials()
-    {
-        $config = Application::$config;
-        self::$_smtpHost     = $config['smtpHost'];
-        self::$_smtpPort     = $config['smtpPort'];
-        self::$_smtpUsername = $config['smtpUsername'];
-        self::$_smtpPassword = $config['smtpPassword'];
     }
 }
